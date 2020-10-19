cd5c5826751ebea51f65ad4e0945b12bf12f4d93
karan <currenshah@gmail.com>
2020-10-19 10:51:00 +0530

    Revision 1

--- app/src/main/java/com/example/tvshowtracker/network/ApiService.kt
+++ app/src/main/java/com/example/tvshowtracker/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.tvshowtracker.network
 
+import com.example.tvshowtracker.responses.TvShowDetailsResponse
 import com.example.tvshowtracker.responses.TvShowsResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,5 +9,10 @@ import retrofit2.http.Query
 interface ApiService{
 
     @GET("most-popular")
-    fun mostPopularTvShows(@Query("page")page:Int): Call<TvShowsResponse>
+    fun mostPopularTvShows(@Query("page")
+                           page:Int): Call<TvShowsResponse>
+
+    @GET("show-details")
+    fun getTvShowDetails(@Query("q")
+                         tvShowId:String):Call<TvShowDetailsResponse>
 }
